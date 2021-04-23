057cdb52c962652bbda115e33298a41f55499f4d
rebecca martinho <rebeccamartinho@hotmail.com>
2021-04-23 10:52:01 -0300

    dia 04

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -25,6 +25,7 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+
   return (
     <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
@@ -60,8 +61,23 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', { 
+    params: {
+    _limit: 2,
+    _sort: 'published_at',
+    _order: 'desc'
+  }
+})
+
+const paths = data.map(episode => {
   return {
-    paths: [],
+    params: {
+      slug: episode.id
+    }
+  }
+})
+  return {
+    paths ,
     fallback: 'blocking'
   }
 }
